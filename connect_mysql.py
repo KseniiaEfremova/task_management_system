288f363d5985e8f5774906d0ddb04b1c083edc9c
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-10-17 11:25:52 +0100

    Add connect_to_mysql_database function to connect to a MySQL database

--- connect_mysql.py
+++ connect_mysql.py
@@ -1,3 +1,27 @@
 import mysql.connector
+from utils import get_user_and_passwd_from_file
 
-db = mysql.connector.connect()
+
+user, password = get_user_and_passwd_from_file("mysql.json")
+
+
+def connect_to_mysql_database():
+    """
+    Connects to a MySQL database using credentials from a JSON file.
+    Returns:
+        mysql.connector.MySQLConnection: A MySQL database connection object.
+    Raises:
+        mysql.connector.Error: If there is an error during the database connection process.
+    """
+    try:
+        # Establish a connection to the MySQL database
+        db_connection = mysql.connector.connect(
+            host="localhost",
+            user=user,
+            passwd=password
+        )
+        return db_connection
+    except mysql.connector.Error as err:
+        # Handle MySQL database connection errors
+        raise err
+
