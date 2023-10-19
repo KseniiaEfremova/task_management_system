91750be64cc1455d3c515f41aca5a4946a6afc1e
Kerri Tanya <kerritg1@gmail.com>
2023-10-19 14:10:35 +0100

    created add_new_task.py file

---
+++ add_new_task.py
@@ -0,0 +1,6 @@
+import mysql.connector
+from mysql.connector import errorcode
+from datetime import datetime
+from connect_mysql import connect_to_mysql_database
+from create_db import get_cursor_and_connection
+
