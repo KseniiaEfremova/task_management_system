eb92d37196d8f5484c1a717bae1081a6ce35c881
Kerri Tanya <kerritg1@gmail.com>
2023-10-19 14:27:10 +0100

    added while True loop to insert_new_task() function in add_new_task.py, where date inputted - will loop if value error until correct date input then break

--- add_new_task.py
+++ add_new_task.py
@@ -15,11 +15,15 @@ def insert_new_task():
         # Get user input
         project_id = int(input("Please enter the Project ID number you would like to add a task to: "))
         description = input("Please enter a description of your task: ")
-        try:
-                deadline_date_input = input("Please enter the date deadline of your task in DD/MM/YYYY format: ")     
+        # while True loop will break if a valid date is entered
+        # or loop if a Value error and ask the user to reenter a valid date
+        while True:
+            try:
+                deadline_date_input = input("Please enter the date deadline of your task in DD/MM/YYYY format: ")
                 deadline_date = datetime.strptime(deadline_date_input, "%d/%m/%Y")
                 formatted_deadline_date = deadline_date.strftime("%Y-%m-%d")
-        except ValueError:
+                break
+            except ValueError:
                 print("Invalid date format. Please use DD/MM/YYYY.")
         status = input("Please select the status of your task - 'todo', 'in progress', 'in review', 'done': ")
         
