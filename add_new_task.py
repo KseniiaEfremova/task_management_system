335eea5b37b110acf8228237d0dbb460abadbbc8
Kerri Tanya <kerritg1@gmail.com>
2023-10-19 14:22:27 +0100

    created function to add a new task in add_new_task.py

--- add_new_task.py
+++ add_new_task.py
@@ -4,3 +4,36 @@ from datetime import datetime
 from connect_mysql import connect_to_mysql_database
 from create_db import get_cursor_and_connection
 
+db_name = 'task_management_system'
+
+def insert_new_task():
+    try:
+        # connecting to database
+        cursor, _ = get_cursor_and_connection(db_name)
+        print(f'Connected to database: {db_name}')
+
+        # Get user input
+        project_id = int(input("Please enter the Project ID number you would like to add a task to: "))
+        description = input("Please enter a description of your task: ")
+        try:
+                deadline_date_input = input("Please enter the date deadline of your task in DD/MM/YYYY format: ")     
+        except ValueError:
+                print("Invalid date format. Please use DD/MM/YYYY.")
+        status = input("Please select the status of your task - 'todo', 'in progress', 'in review', 'done': ")
+        
+        # Query
+        query = """INSERT INTO {db_name} ({}) VALUES ('{}', '{}', '{}', '{}')""", (project_id, description, deadline_date_input, status)
+        cursor.execute(query)
+        db_connection.commit()
+        cursor.close()
+        print("\nYour task has been successfully entered into the database!")
+
+    except Exception as exc:
+        print(exc)
+
+    finally:
+        if db_connection:
+            db_connection.close()
+            print("Connection closed")
+
+insert_new_task()
