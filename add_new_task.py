eeb991e03385a96cf1366ca5c86ed7d58e16c14b
Kerri Tanya <kerritg1@gmail.com>
2023-10-19 14:44:03 +0100

    fixed db_connection error in add_new_task.py

--- add_new_task.py
+++ add_new_task.py
@@ -1,7 +1,6 @@
 import mysql.connector
 from mysql.connector import errorcode
 from datetime import datetime
-from connect_mysql import connect_to_mysql_database
 from create_db import get_cursor_and_connection
 
 db_name = 'task_management_system'
@@ -9,7 +8,7 @@ db_name = 'task_management_system'
 def insert_new_task():
     try:
         # connecting to database
-        cursor, _ = get_cursor_and_connection(db_name)
+        cursor, db_connection = get_cursor_and_connection(db_name)
         print(f'Connected to database: {db_name}')
 
         # Get user input
