279ffb34ee0a39b49b5fd9cff1561599f2b1a598
Kerri Tanya <kerritg1@gmail.com>
2023-10-20 17:31:20 +0100

    edited variables in add_new_task function in db_utils.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from db_utils import get_all_projects, DB_NAME
+from db_utils import get_all_projects, add_new_task, DB_NAME
 
 
 app = Flask(__name__)
@@ -16,6 +16,18 @@ def get_projects():
     project_table = "projects"
     res = dict(get_all_projects(DB_NAME, project_table))
     return jsonify(res)
+
+
+@app.route("/newtask", methods=['PUT'])
+def adding_task():
+    new_task = request.get_json()
+    {
+        "project_id": input_project_id,
+        "description": input_description,
+        "deadline": formatted_deadline_date,
+        "status": input_status,
+    }
+
 
 
 if __name__ == '__main__':
--- db_utils.py
+++ db_utils.py
@@ -91,15 +91,16 @@ def get_all_projects(db_name, table_name):
     return projects
 
 
-def add_new_task(db_name):
+def add_new_task(project_id, description, deadline, status):
     try:
+        db_name = 'task_management_system' 
         # connecting to database
         cursor, db_connection = get_cursor_and_connection(db_name)
         print(f'Connected to database: {db_name}')
 
         
         # Query
-        query = """INSERT INTO tasks ({}) VALUES ('{}', '{}', '{}', '{}')""".format(input_project_id, input_description, formatted_deadline_date, input_status)
+        query = """INSERT INTO tasks ({}) VALUES ('{}', '{}', '{}', '{}')""".format(project_id, description, deadline, status)
         cursor.execute(query)
         db_connection.commit()
         cursor.close()
