8c5e8aa131f1058640ab565117b8213b7b2a306b
nataliapiotrowska <nataliapiotrowska@MacBook-Air-Natalia.local>
2023-10-20 15:59:28 +0200

    not fully tested yet

--- db_utils.py
+++ db_utils.py
@@ -1,5 +1,31 @@
 import mysql.connector
-from config import USER, HOST, PASSWORD
+from create_db import get_cursor_and_connection
+
 
 def delete_task():
-    return deleted
+    try:
+        db_name = 'task_management_system'
+        cursor, db_connection = get_cursor_and_connection(db_name)
+        print(f"Connected to database {db_name}")
+
+        # Get user input
+        task_id = input('Please enter the Task ID of the task you would like to delete: ')
+        task_id = int(task_id)  # Convert the input from str to int
+
+        # Query deleting the task with ID provided by the user from the db
+        query = """DELETE FROM tasks WHERE TASK_id = '{x}'""".format(x=task_id)
+        cursor.execute(query)
+        db_connection.commit()
+
+        cursor.close()
+        print('Your task has been deleted')
+
+    except Exception as exc:
+        print(exc)
+
+    finally:
+        if db_connection:
+            db_connection.close()
+
+
+delete_task()
--- delete_task.py
+++ delete_task.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify
+from db_utils import delete_task
 
 app = Flask(__name__)
 
-@app.route('/delete_task')
-def delete_task():
-    return 'deleted'
+@app.route('/delete_task', methods=['DELETE'])
+def delete_task_route():
+    result = delete_task()
+    return jsonify(result)
 
-if __name__ == '__main__':
-    app.run(debug=True)
