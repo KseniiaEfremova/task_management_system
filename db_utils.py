a7f5d6b78ce508db3f619b9686013cc2a26e8b09
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-10-20 12:47:20 +0100

    Add one more argument in get_all_projects, delete redundant exception

--- db_utils.py
+++ db_utils.py
@@ -1,48 +1,10 @@
 import mysql.connector
-import json
 import logging
 from mysql.connector import errorcode
+from config import data
 
+host, user, password = data["host"], data["user"], data["passwd"]
 DB_NAME = 'task_management_system'
-
-
-def get_host_user_and_passwd_from_file(file_name: str) -> tuple:
-    """
-    Retrieves user and password information from a JSON configuration file.
-    Args:
-        file_name (str): The name of the JSON configuration file.
-    Returns:
-        tuple: A tuple containing host (str), user (str) and password (str) extracted from the file.
-    Raises:
-        ValueError: If the retrieved credentials (user or password) are empty or None.
-        FileNotFoundError: If the specified file does not exist.
-        KeyError: If the "user" or "passwd" keys are missing in the JSON data.
-        json.JSONDecodeError: If the file content is not a valid JSON.
-    """
-    try:
-        with open(file_name, 'r') as config_file:
-            data = json.load(config_file)
-            host = data["host"]
-            user = data["user"]
-            passwd = data["passwd"]
-        if not host or not user or not passwd:
-            # Print an error message and raise a ValueError if user or password or host is empty or None
-            print("Invalid user or password or host in JSON configuration.")
-            raise ValueError("Invalid user or password or host in JSON configuration.")
-        return host, user, passwd
-    except FileNotFoundError as e:
-        print(f"The file '{file_name}' does not exist.")
-        raise e
-    except (KeyError, json.JSONDecodeError) as e:
-        print(f"Error reading JSON data from '{file_name}': {e}")
-        raise e
-    except Exception as e:
-        # Catch unexpected exceptions, log them, and raise to crash the program
-        logging.exception(f"Unexpected error occurred: {e}")
-        raise e
-
-
-host, user, password = get_host_user_and_passwd_from_file("mysql.json")
 
 
 def connect_to_mysql_database(db_name):
@@ -109,26 +71,18 @@ def connect_to_database_or_create_if_not_exists(db_name):
 connect_to_database_or_create_if_not_exists(DB_NAME)
 
 
-class DbConnectionError(Exception):
-    pass
-
-
-def get_all_projects(db_name):
+def get_all_projects(db_name, table_name):
     projects = []
     try:
         cursor, db_connection = get_cursor_and_connection(db_name)
         print("Connected to DB: %s" % db_name)
 
-        query = """SELECT project_id, project_name FROM projects"""
+        query = """SELECT project_id, project_name FROM {}""".format(table_name)
 
         cursor.execute(query)
 
         projects = cursor.fetchall()
         cursor.close()
-
-    except Exception:
-        raise DbConnectionError("Failed to read data from DB")
-
     finally:
         if db_connection:
             db_connection.close()
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from db_utils import get_all_projects, DB_NAME
 
 
-def display_projects():
-    projects = get_all_projects(DB_NAME)
+def display_projects(table_name):
+    projects = get_all_projects(DB_NAME, table_name)
     print("################################\n")
     print("All projects:")
     number_of_project = 1
