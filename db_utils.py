0631e6cb9f67df1da2b9fb618f420e8b0014d66f
Kerri Tanya <kerritg1@gmail.com>
2023-10-20 15:47:02 +0100

    deleted separate add_new_task.py file and added add_new_task.py function to db_utils.py currently input in the function, will move this out to run() in main.py later

--- db_utils.py
+++ db_utils.py
@@ -89,3 +89,40 @@ def get_all_projects(db_name, table_name):
             print("DB connection is closed")
 
     return projects
+
+
+def add_new_task(db_name):
+    try:
+        # connecting to database
+        cursor, db_connection = get_cursor_and_connection(db_name)
+        print(f'Connected to database: {db_name}')
+
+        # Get user input
+        project_id = int(input("Please enter the Project ID number you would like to add a task to: "))
+        description = input("Please enter a description of your task: ")
+        # while True loop will break if a valid date is entered
+        # or loop if a Value error and ask the user to reenter a valid date
+        while True:
+            try:
+                deadline_date_input = input("Please enter the date deadline of your task in DD/MM/YYYY format: ")
+                deadline_date = datetime.strptime(deadline_date_input, "%d/%m/%Y")
+                formatted_deadline_date = deadline_date.strftime("%Y-%m-%d")
+                break
+            except ValueError:
+                print("Invalid date format. Please use DD/MM/YYYY.")
+        status = input("Please select the status of your task - 'todo', 'in progress', 'in review', 'done': ")
+        
+        # Query
+        query = """INSERT INTO tasks ({}) VALUES ('{}', '{}', '{}', '{}')""", (project_id, description, formatted_deadline_date, status)
+        cursor.execute(query)
+        db_connection.commit()
+        cursor.close()
+        print("\nYour task has been successfully entered into the database!")
+
+    except Exception as exc:
+        print(exc)
+
+    finally:
+        if db_connection:
+            db_connection.close()
+            print("Connection closed")
