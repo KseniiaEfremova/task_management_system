15a5e6fa33cd23f805c985a56da2b98eb673c7f7
Sadaf Hamidi <sadaf@Sadafs-MacBook-Air.local>
2023-10-23 21:00:15 +0100

    modified delete_project on main.py

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ app.secret_key = 'paskudzio'
 def home():
     return render_template('index.html')
 
-def delete_project(project_name):
-    response = requests.delete(f"http://127.0.0.1:5001/delete_project/<int:project_name>")
+def delete_project(project_id):
+    response = requests.delete(f"http://127.0.0.1:5001/delete_project/<int:project_id>")
     if response.status_code == 200:
-        print(f"Project: {project_name} has successfully been deleted")
+        print(f"Project: {project_id} has successfully been deleted")
     else:
         print(f"Failed to delete project.")
+
 
 def run():
         try:
