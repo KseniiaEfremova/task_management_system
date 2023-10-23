3b59b2a2653478a5a8d79bff898fe27e6a52b2f5
k.stopczynska <k.stopczynska@gmail.com>
2023-10-23 11:13:30 +0200

    added error handling routes

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, make_response
 from db_utils import get_all_projects, DB_NAME
 
 
 app = Flask(__name__)
-app.secret_key = 'paskudzio'
 
+
+@app.errorhandler(404)
+def handle_404(error):
+    response = make_response(jsonify({'error': 'page not found'}), 404)
+    return response
+
+@app.errorhandler(500)
+def handle_500(error):
+    response = make_response(jsonify({'error': 'server is down, contact one of Coding Stars United Developer'}), 500)
+    return response
 
 @app.route('/')
 def home():
